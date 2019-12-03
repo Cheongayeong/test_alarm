53be119bf4b262dc943fa8e4e4229072f0dbc6d8
jw0712 <jiwonchoi712@gmail.com>
2019-12-03 16:14:56 +0900

    Map, Message, Calendar

---
+++ app/src/main/java/com/example/alarmproject/MessageActivity.java
@@ -0,0 +1,74 @@
+package com.example.alarmproject;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MessageActivity extends AppCompatActivity {
+
+    Button buttonSend;
+    EditText textPhoneNo;
+    EditText textSMS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+        textPhoneNo = (EditText) findViewById(R.id.editTextPhoneNo);
+        textSMS = (EditText) findViewById(R.id.editTextSMS);
+
+        //버튼클릭
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String phoneNo = textPhoneNo.getText().toString();
+                String sms = textSMS.getText().toString();
+
+                try {
+                    //전송
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "전송 완료!", Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "SMS faild, please try again later!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("SKKU", "Permission is not granted, requesting");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, 123);
+            buttonSend.setEnabled(false);
+        } else {
+            Log.d("SKKU", "Permission is granted");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("SKKU", "Permission has been granted");
+                buttonSend.setEnabled(true);
+            } else {
+                Log.d("SKKU", "Permission has been denied or request cancelled");
+                buttonSend.setEnabled(false);
+            }
+        }
+    }
+
+
+
+}
